dc5e0bfc7066ba53c5a80ad3b7847e00e3da6e70
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-07-11 08:43:24 +0900

    Create Prac3.cpp

---
+++ NestedLoop/NestedLoop/Prac3.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main() {
+
+	int input;
+	printf("숫자를 입력하세요 : ");
+	fseek(stdin, 0, SEEK_END);
+	scanf_s("%d", &input);
+
+	for (int i = 0; i < input; i++)
+	{
+		for (int k = 0; k < input - i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
