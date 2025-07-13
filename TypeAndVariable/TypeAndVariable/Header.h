2818f935d9fc89c398185678dc802db783096dc8
SUROM <opp92@naver.com>
2025-07-13 14:26:34 +0900

    Update Header.h

--- TypeAndVariable/TypeAndVariable/Header.h
+++ TypeAndVariable/TypeAndVariable/Header.h
@@ -4,3 +4,17 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+
+float inputValue();
+
+float add(float a, float b);
+
+float sub(float a, float b);
+
+float mul(float a, float b);
+
+float div(float a, float b);
+
+float mod(float a, float b);
+
+char getoper();
