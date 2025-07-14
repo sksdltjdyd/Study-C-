4e7fc45eba90b52295d2a70ed1a2beb7330edfd1
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-07-14 09:06:14 +0900

    Create Header.h

---
+++ String/String/Header.h
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS //scanf_s대신 scanf 사용하게 해주는 선언문
+
+#include <cstdio>
+#include <cstring>
+/*
+// C 스타일 문자열을 다루기 위한 헤더
+// strcpy(...) : 문자열 복사
+// strlen(...) : 문자열 길이 반환
+// strcmp(...) : 문자열 비교
+// strcat(...) : 문자열 연결
+// strchr(...) : 문자열에서 특정 문자 찾기
+// strrchr(...) : 문자열에서 특정 문자의 마지막 위치 찾기
+// strpos(...) : 문자열에서 특정 문자열 찾기
+*/
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
