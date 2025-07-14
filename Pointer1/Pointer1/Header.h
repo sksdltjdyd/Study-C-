ce00e2520d4671c43ccc54e5f13884a757de94d9
SUROM <62508307+sksdltjdyd@users.noreply.github.com>
2025-07-15 08:38:13 +0900

    Update Header.h

--- Pointer1/Pointer1/Header.h
+++ Pointer1/Pointer1/Header.h
@@ -6,3 +6,17 @@
 #include <cstring>
 #include <ctime>
 #include <iostream>
+
+int GetInput();
+
+int sum(int a, int b);
+
+
+int sub(int a, int b);
+
+
+int mul(int a, int b);
+
+int Div(int a, int b);
+
+char GetOper();
