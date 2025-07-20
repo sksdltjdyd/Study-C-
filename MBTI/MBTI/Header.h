7c4b834819c11501dbd530937622f2a39fa9e7e5
SUROM <opp92@naver.com>
2025-07-20 14:41:42 +0900

    Create Header.h

---
+++ MBTI/MBTI/Header.h
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+
+int GetQuestion(const char* q, int v1, int v2, int v3, int v4);
+
+void PrintResult(int v1, int v2, int v3, int v4);
