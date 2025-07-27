2dca61a3b0ce1a6ae7ba5bea331daf7f68f73008
SUROM <opp92@naver.com>
2025-07-27 20:17:03 +0900

    Update Main.cpp

--- Chapter3-9-10_Tetris/Chapter3-9-10_Tetris/Main.cpp
+++ Chapter3-9-10_Tetris/Chapter3-9-10_Tetris/Main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
 
+	srand(time(0));
 	// 게임이 실행된 이후 지나는 시간을 지속적으로 체크
 	float prev = (float)clock() / CLOCKS_PER_SEC;
 
@@ -13,6 +14,7 @@ int main() {
 
 	// 게임 엔진을 불러와서 관리
 	GameEngine* gameengine = new GameEngine();
+	gameengine->init();
 
 	while (true)
 	{
@@ -26,24 +28,39 @@ int main() {
 
 		// 방향키 설정
 		bool left = keyState('a');
-		if (left = true) 
+		bool right = keyState('d');
+		bool down = keyState('s');
+		bool rotate = keyState('w');
+		if (left == true) 
 		{
 			// 왼쪽으로 블록 이동
+			gameengine->next(dt, 'a');
+		}
+		else if (right == true) {
+			gameengine->next(dt, 'd');
+		}
+		else if (down == true) {
+			gameengine->next(dt, 's');
+		}
+		else if (rotate == true) {
+			gameengine->next(dt, 'w');
 		}
 		else 
 		{
 			// 블록 떨어짐
+			gameengine->next(dt, 0);
 		}
 
-		bool right = keyState('d');
+		
 
-		gameengine->next(dt, 0);
+		
 
 		// 화면 출력
 		gameengine->makedisplaydata();
 		display->draw();
 
 		// 게임 상태 판별
+		if (gameengine->state == GameEngine::GameState::GAMEOVER) break;
 		
 	}
 
