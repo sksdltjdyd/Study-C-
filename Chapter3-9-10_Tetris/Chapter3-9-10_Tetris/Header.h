a3afc4c783196fb7332d341903bd3d3c2580188d
SUROM <opp92@naver.com>
2025-07-28 21:23:33 +0900

    Update Header.h

--- Chapter3-9-10_Tetris/Chapter3-9-10_Tetris/Header.h
+++ Chapter3-9-10_Tetris/Chapter3-9-10_Tetris/Header.h
@@ -47,6 +47,7 @@ public:
 class GameEngine
 {
 public:
+	// 게임의 종료 조건 설정
 	enum class GameState
 	{
 		PLAYING, GAMEOVER,
@@ -104,12 +105,14 @@ public:
 		if (elapsed >= 0.5f)
 		{
 			if (CanGoDown()) {
+				// can go down 함수가 참이면 블록 위치가 계속 더해져서 내려간다
 				blocky++;
 			}
 			else
 			{
 				// userblock을 gamegrid data에 전사
 				trans();
+				// 게임오버 함수가 참이면 게임오버 상태 설정
 				if (gameoverDecision()) state = GameState::GAMEOVER;
 			}
 
@@ -141,13 +144,16 @@ public:
 		{
 			for (int k = 0; k < USERBLOCK_SIZE; k++) {
 				if (userBlock[i][k] == 1 && i + blocky + 1 >= GRID_HEIGHT) {
+					// 전사된 그리드의 블록이 존재하고 블록의 y축 위치 + 1이 전사된 그리드 높이보다 크면 더 내려가지 않는다
 					return false;
 				}
 				if (userBlock[i][k] == 1 && gameGridData[i + blocky + 1][k + blockx] == 1) {
+					//  전사된 그리드의 블록이 존재하고 현재 전사된 그리드의 블록 바로 아래에 블록이 있다면 더 내려가지 않는다
 					return false;
 				}
 			}
 		}
+		// 두가지 경우가 아니면 내려간다
 		return true;
 	}
 
@@ -157,13 +163,16 @@ public:
 		{
 			for (int k = 0; k < USERBLOCK_SIZE; k++) {
 				if (userBlock[i][k] == 1 && k + blockx - 1 < 0) {
+					// 블록이 움직인 위치에 블록이 존재하거나 블록 위치를 k축으로 1 뺐더니 0보다 작으면 전사된 배열을 벗어나는거므로 움직이면 안됀다
 					return false;
 				}
 				if (userBlock[i][k] == 1 && gameGridData[i + blocky][k + blockx - 1] == 1) {
+					// 블록이 움직인 위치에 블록이 존재하거나 전사된 데이터의 블록 위치의 왼쪽에 블록이 있다면 움질일 수 없다
 					return false;
 				}
 			}
 		}
+		// 두가지 경우가 아니면 왼쪽으로 움직인다
 		return true;
 	}
 
@@ -174,13 +183,16 @@ public:
 		{
 			for (int k = 0; k < USERBLOCK_SIZE; k++) {
 				if (userBlock[i][k] == 1 && k + blockx + 1 > GRID_WIDTH - 1) {
+					// 블록의 움직인 위치에 블록이 존재하거나 블록 위치를 k축으로 1 더했더니 배열그리드 넓이에서 1 뺀것보다 크면 배열을 벗어나는것이므로 움직일 수 없다
 					return false;
 				}
 				if (userBlock[i][k] == 1 && gameGridData[i + blocky][k + blockx + 1] == 1) {
+					// 블록의 움직인 위치에 블록이 존재하거나 전사된 데이터의 블록 위치에 블록이 있다면 움질일 수 없다
 					return false;
 				}
 			}
 		}
+		// 두가지 경우가 아니면 오른쪽으로 움직인다
 		return true;
 	}
 
@@ -188,23 +200,27 @@ public:
 	bool isLineFilled(int y) {
 		for (int i = 0; i < GRID_WIDTH; i++)
 		{
+			// 가득 차 있다면 한칸이라도 0이면 안되기에 지워지면 안됀다
 			if (gameGridData[y][i] == 0) return false;
 		}
 		return true;
 	}
 
+	// 가득차 있는 라인을 지운다
 	void eraseLine(int y) {
 		for (int i = 0; i < GRID_WIDTH; i++)
 		{
+			// y번째 줄 라인을 전부 지운다
 			gameGridData[y][i] = 0;
 		}
 	}
 
-	// y 좌표를 기준으로 한칸씩 아래로 내림
+	// 라인을 지운 뒤 y 좌표를 기준으로 한칸씩 아래로 내림
 	void drop(int y) {
 		for (int i = y; i >= 0; i--)
 		{
 			for (int k = 0; k < GRID_WIDTH; k++) {
+				// 전사된 블록에 i - 1 위치에 넣었는데 그 값이 0보다 작다면 배열 그리드를 벗어나는것이므로 그냥 0을 넣고 그게 아니라면 한칸씩 땡긴 값을 넣는다
 				gameGridData[i][k] = i - 1 < 0 ? 0 : gameGridData[i - 1][k];
 			}
 		}
@@ -217,6 +233,8 @@ public:
 			for (int k = 0; k < USERBLOCK_SIZE; k++) {
 				int _x = k + blockx;
 				int _y = i + blocky;
+				// 둘다 아닐 시, 유저블록이 게임 화면 안에 있는것이므로 데이터를 넣어준다
+				// 디스플레이데이터 x,y좌표가 기존에 0인지 1인지 판단을 해서 1이면 유저블록 x,y좌표를 넣어주고 0이면 새로운 블록 데이터를 넣어준다
 				// gameGridData[i + blocky][k + blockx] = userBlock[i][k] == 1 ? userBlock[i][k] : gameGridData[i + blocky][k + blockx];
 				gameGridData[_y][_x] = userBlock[i][k] | gameGridData[_y][_x];
 			}
@@ -225,6 +243,7 @@ public:
 		for (int i = 0; i < GRID_HEIGHT; i++)
 		{
 			if (isLineFilled(i)) {
+				// 라인이 가득차면 가득 찬 라인을 지우고 블록을 한칸씩 떙긴다
 				eraseLine(i);
 				drop(i);
 			}
@@ -233,11 +252,13 @@ public:
 		makeuserblock();
 	}
 
+	// 게임 종료 조건 설정
 	bool gameoverDecision() {
 		for (int i = 0; i < USERBLOCK_SIZE; i++)
 		{
 			for (int k = 0; k < USERBLOCK_SIZE; k++) {
 				if (userBlock[i][k] == 1 && gameGridData[i + blocky][k + blockx] == 1) {
+					// 유저블록의 위치에 블록이 있고 전사된 그리드 데이터에 블록이 존재하면 참 
 					return true;
 				}
 			}
@@ -247,6 +268,7 @@ public:
 
 	// 새로운 블록 생성
 	void makeuserblock() {
+		// 블록이 최초 생성되는 위치 설정
 		blockx = GRID_WIDTH / 2 - USERBLOCK_SIZE / 2;
 		blocky = 0;
 
