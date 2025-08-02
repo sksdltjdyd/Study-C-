91eefd334b7048c973be2d7627bef67f63096c77
SUROM <opp92@naver.com>
2025-08-02 20:27:56 +0900

    Create TetrisHelper.h

---
+++ Chapter4-3/Chapter4-3/TetrisHelper.h
@@ -0,0 +1,16 @@
+#ifndef __TETRIS_HELPER_H__
+#define __TETRIS_HELPER_H__
+
+#include <Windows.h>
+#include <cstdio>
+
+
+void putStringOnPosition(int x, int y, const char* content);
+
+void showConsoleCursor(bool showFlag);
+
+void drawPosition(int x, int y, bool filled);
+
+bool keyState(char c);
+
+#endif // !__TETRIS_HELPER_H__
